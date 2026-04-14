629ce8ab27d9a06ec87f454421f7965e4a05a890
Samuel Beland-Leblanc <samuel@sbl-dev.ca>
2026-04-13 21:17:43 -0400

    Adds chunked rules

--- src/mtg_agent/cli/preprocess.py
+++ src/mtg_agent/cli/preprocess.py
@@ -4,6 +4,10 @@ import itertools as it
 from pathlib import Path
 
 import click
+import numpy as np
+from transformers import AutoTokenizer
+from langchain_text_splitters import MarkdownHeaderTextSplitter
+
 
 @click.group()
 def cli():
@@ -27,7 +31,7 @@ def convert_rules_to_md(input_rules: Path, output_md: Path):
 
     h2_regex = re.compile(r'^\d\. ')
     h3_regex = re.compile(r'^\d{3}\. ')
-    nested_regex = re.compile(r'^\d{3}\.\d[a-z] ')
+    h4_regex = re.compile(r'^\d{3}\.\d+\. ')
 
     markdown_content = ["# Magic: The Gathering Comprehensive Rules"]
 
@@ -42,17 +46,70 @@ def convert_rules_to_md(input_rules: Path, output_md: Path):
         ):
             if line == "Glossary":
                 break
+
             if h2_regex.match(line):
                 markdown_content.append(f"## {line}")
             elif h3_regex.match(line):
                 markdown_content.append(f"### {line}")
-            elif nested_regex.match(line):
-                markdown_content.append(f"  - {line}")
+            elif h4_regex.match(line):
+                markdown_content.append(f"#### {line}")
             else:
                 markdown_content.append(f"- {line}")
 
     with output_md.open(mode="w") as f:
         f.write("\n".join(markdown_content))
+
+
+@cli.command()
+@click.option("-e", "--embedding-model", default="google/gemma-4-E4B")
+@click.argument('input_rules_md', type=click.Path(exists=True, path_type=Path))
+@click.argument('chunks_json', type=click.Path(path_type=Path))
+def chunk_rules(
+        embedding_model: str,
+        input_rules_md: Path,
+        chunks_json: Path
+):
+    tok = AutoTokenizer.from_pretrained(embedding_model)
+
+    with input_rules_md.open(mode="r") as f:
+        markdown_rules = f.read()
+
+    headers_to_split_on = [
+        ("#", "h1"),
+        ("##", "h2"),
+        ("###", "h3"),
+        ("####", "h4"),
+    ]
+
+    markdown_splitter = MarkdownHeaderTextSplitter(headers_to_split_on, strip_headers=False)
+    rules_splits = markdown_splitter.split_text(markdown_rules)
+
+    chunks = [
+        {
+            "h1": chunk.metadata.get("h1", ""),
+            "h2": chunk.metadata.get("h2", ""),
+            "h3": chunk.metadata.get("h3", ""),
+            "h4": chunk.metadata.get("h4", ""),
+            "content": chunk.page_content
+        }
+        for chunk in rules_splits
+    ]
+
+    token_counts = np.array([
+        len(tok.tokenize(
+            f"{chunk['h2']}\n{chunk['h3']}\n{chunk['h4']}\n{chunk['content']}"
+        ))
+        for chunk in chunks
+    ])
+
+    print(np.quantile(token_counts, 0.))
+    print(np.quantile(token_counts, 0.25))
+    print(np.quantile(token_counts, 0.5))
+    print(np.quantile(token_counts, 0.75))
+    print(np.quantile(token_counts, 1.0))
+
+    with chunks_json.open(mode="w") as f:
+        json.dump(chunks, f, indent=2)
 
 
 if __name__ == '__main__':
